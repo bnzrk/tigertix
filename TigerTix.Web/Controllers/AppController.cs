6864287b3e6fefa4dfe296829a8e602f035ef79d
Ben <127999784+bnzrk@users.noreply.github.com>
2024-09-06 10:12:33 -0400

    Add view and controller

---
+++ TigerTix.Web/Controllers/AppController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TigerTix.Web.Controllers
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            Console.WriteLine("Index action method invoked");
+            return View();
+        }
+    }
+}
--- TigerTix.Web/Program.cs
+++ TigerTix.Web/Program.cs
@@ -1,6 +1,25 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+builder.Services.AddControllers();
+
 var app = builder.Build();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapRazorPages();
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=App}/{action=Index}/{id?}");
 
 app.UseDefaultFiles();
 app.UseStaticFiles();
+
 app.Run();
