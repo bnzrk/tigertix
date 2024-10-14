dcdd4f395b4de8c7f5088187505ba814969ac1f8
randallreed2004 <randallreed2004@gmail.com>
2024-10-13 20:35:03 -0400

    Implemented the controller implementation for events

--- TigerTix.Web/Controllers/AppController.cs
+++ TigerTix.Web/Controllers/AppController.cs
@@ -9,9 +9,12 @@ namespace TigerTix.Web.Controllers
     {
         private readonly IUserRepository _userRepository;
 
-        public AppController(IUserRepository userRepository)
+        private readonly IEventRepository _eventRepository;
+
+        public AppController(IUserRepository userRepository, IEventRepository eventRepository)
         {
             _userRepository = userRepository;
+            _eventRepository = eventRepository;
         }
 
         public IActionResult Index()
@@ -34,6 +37,49 @@ namespace TigerTix.Web.Controllers
                           select u;
 
             return View(results.ToList());
+        }
+
+
+        public IActionResult ShowEvents() {
+            var events = _eventRepository.GetAllEvents();
+            return View(events.ToList());
+        }
+        
+        [HttpGet]
+        public IActionResult CreateEvent() {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateEvent(Event model) {
+            if (ModelState.IsValid) {
+                _eventRepository.SaveEvent(model);
+                _eventRepository.SaveAll();
+
+                return RedirectToAction("ShowEvents");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult UpdateEvent(int id) {
+            var eventModel = _eventRepository.GetEventByID(id);
+            if (eventModel == null) {
+                return NotFound();
+            }
+            return View(eventModel);
+        }
+
+        public IActionResult DeleteEvent(int id) {
+            var eventModel = _eventRepository.GetEventByID(id);
+            if (eventModel == null) {
+                return NotFound();
+            }
+
+            _eventRepository.DeleteEvent(eventModel);
+            _eventRepository.SaveAll();
+
+            return RedirectToAction("ShowEvents");
         }
     }
 }
