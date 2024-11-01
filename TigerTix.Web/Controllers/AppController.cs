990e951cfc550bacb3447d7f50725e6874f97542
Ben <127999784+bnzrk@users.noreply.github.com>
2024-11-01 18:01:29 -0400

    Update login and register actions

--- TigerTix.Web/Controllers/AppController.cs
+++ TigerTix.Web/Controllers/AppController.cs
@@ -7,6 +7,7 @@ using System.Security.Claims;
 using TigerTix.Web.Data;
 using TigerTix.Web.Data.Entities;
 using TigerTix.Web.Models;
+using System.Text.RegularExpressions;
 
 namespace TigerTix.Web.Controllers
 {
@@ -30,44 +31,86 @@ namespace TigerTix.Web.Controllers
 
         public async Task<IActionResult> Index()
         {
-            await _signInManager.SignOutAsync();
-            HttpContext.Response.Cookies.Delete(".AspNetCore.Cookies");
+            var user = await GetCurrentUserAsync();
 
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-            var user = await _userManager.FindByIdAsync(userId);
-
-            Console.WriteLine("Logged in as: " + user.FirstName + " " + user.LastName);
+            if (user != null)
+                Console.WriteLine("Logged in as: " + user.FirstName + " " + user.LastName);
+            else
+                Console.WriteLine("Not logged in.");
 
             return View();
+        }
+
+        public IActionResult Login()
+        {
+            // Redirect if a user is already logged in
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpPost("App/Login")]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            // Redirect if a user is already logged in
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                return RedirectToAction("Index");
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(model.UserName, model.Password, model.RememberMe, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
         }
 
         public IActionResult Register()
         {
+            // Redirect if a user is already logged in
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                return RedirectToAction("Index");
+            }
+
             return View();
         }
-
-        //[HttpPost("App/Register")]
-        //public IActionResult Register(RegisterViewModel model)
-        //{
-        //    User user = new User();
-        //    user.UserName = model.UserName;
-        //    user.FirstName = model.FirstName;
-        //    user.LastName = model.LastName;
-        //    _userRepository.SaveUser(user);
-        //    _userRepository.SaveAll();
-
-        //    return View();
-        //}
 
         [HttpPost("App/Register")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
+            // Redirect if a user is already logged in
+            if (User.Identity?.IsAuthenticated == true)
+            {
+                return RedirectToAction("Index");
+            }
+
+            // Ensure valid CUID format in model
+            string pattern = @"^C\d{6}$";
+            if (!Regex.IsMatch(model.UserName ?? "", pattern))
+            {
+                ModelState.AddModelError("UserName", "Invalid CUID");
+            }
+
+            // Ensure all required fields are filled in model
             if (!ModelState.IsValid)
             {
                 return View(model);
             }
 
+            // Attempt to create the new user and sign in on success
             ApplicationUser user = new ApplicationUser
             {
                 UserName = model.UserName,
@@ -75,22 +118,29 @@ namespace TigerTix.Web.Controllers
                 FirstName = model.FirstName,
                 LastName = model.LastName
             };
-
             var result = await _userManager.CreateAsync(user, model.Password);
             if (result.Succeeded)
             {
                 await _signInManager.SignInAsync(user, isPersistent: false);
-                return RedirectToAction("");
+                return RedirectToAction("Index");
             }
 
+            // Grab all error messages from attempting to create the user and add to model state
             foreach (var error in result.Errors)
             {
-                ModelState.AddModelError(string.Empty, error.Description);
+                if (error.Code == "DuplicateUserName")
+                {
+                    // Reword the duplicate username error message
+                    ModelState.AddModelError("UserName", "An account with this CUID is already registered.");
+                }
+                else
+                    ModelState.AddModelError(string.Empty, error.Description);
             }
 
             return View(model);
         }
 
+        // TODO: Obsolete. Remove
         public IActionResult ShowUsers()
         {
             var results = from u in _userRepository.GetAllUsers()
@@ -100,7 +150,8 @@ namespace TigerTix.Web.Controllers
         }
 
 
-        public IActionResult ShowEvents() {
+        public IActionResult ShowEvents() 
+        {
             var results = from e in _eventRepository.GetAllEvents()
                           select e;
 
@@ -113,7 +164,8 @@ namespace TigerTix.Web.Controllers
         }
 
         [HttpPost("App/CreateEvent")]
-        public IActionResult CreateEvent(Event eventListing) {
+        public IActionResult CreateEvent(Event eventListing) 
+        {
             if (ModelState.IsValid) {
                 _eventRepository.SaveEvent(eventListing);
                 _eventRepository.SaveAll();
@@ -121,34 +173,12 @@ namespace TigerTix.Web.Controllers
             return View();
         }
 
-        public IActionResult Login()
+        // TODO: Rework to avoid call to database
+        protected async Task<ApplicationUser> GetCurrentUserAsync()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+            return user;
         }
-
-        [HttpPost("App/Login")]
-        public IActionResult Login(LoginViewModel model)
-        {
-            var user = _userRepository.GetUserByUsername(model.UserName);
-            if (user != null)
-            {
-                HttpContext.Session.SetString("Username", user.UserName);
-                Console.WriteLine("Logged in as " + user.FirstName + " " + user.LastName);
-                return RedirectToAction("Login");
-            }
-
-            return View();
-        }
-
-        User GetSessionUser()
-        {
-            var sessionUsername = HttpContext.Session.GetString("Username");
-            if (sessionUsername == null)
-                return null;
-
-            var sessionUser = _userRepository.GetUserByUsername(sessionUsername);
-            return sessionUser;
-        }
-
     }
 }
