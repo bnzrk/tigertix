f4e5055a8251cdfc489a18c62352713e782059c5
randallreed2004 <randallreed2004@gmail.com>
2024-11-03 20:14:10 -0500

    Scratch the previous commit

--- TigerTix.Web/Models/TicketViewModel.cs
+++ TigerTix.Web/Models/TicketViewModel.cs
@@ -4,8 +4,6 @@ namespace TigerTix.Web.Models
 {
     public class TicketViewModel
     {
-        public int Id { get; set; }
-        [Required]
         public int CUID { get; set; }
         [Required]
         public string TicketEvent { get; set; }
